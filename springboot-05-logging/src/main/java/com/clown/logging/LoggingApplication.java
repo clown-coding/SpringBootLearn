8e8f6fb176255fe2ec1dfa3496b639f40d1a1217
Richard <ljy8953898@gmail.com>
2019-01-13 17:46:30 +0800

    添加:SpringBoot日志框架

---
+++ springboot-05-logging/src/main/java/com/clown/logging/LoggingApplication.java
@@ -0,0 +1,16 @@
+package com.clown.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Richard·Ackerman
+ * @create: 2019/1/13
+ * @description: 启动类
+ **/
+@SpringBootApplication
+public class LoggingApplication {
+    public static void main(String[] args){
+        SpringApplication.run(LoggingApplication.class,args);
+    }
+}
